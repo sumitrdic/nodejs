05ba1407b70c82ee0e78da2ba6dbc7eb2dfea065
sumitrdic <33592600+sumitrdic@users.noreply.github.com>
2022-08-18 14:22:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const app = require('express')(); 
-const http = require('http').Server(app); 
-const io = require('socket.io')(http); 
+const express = require('express')
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 3000; 
-app.get('/', (req, res) => 
-{ res.sendFile(__dirname + '/index.html'); });
- io.on('connection', (socket) => {
-      socket.on('chat message', msg => { io.emit('chat message', msg); }); }); http.listen(port, () => { console.log(`Socket.IO server running at http://localhost:${port}/`); });
+app.use(express.static(__dirname + '/Myweb/'));
+    
+ io.on('connection', (socket) => { 
+ 
+ socket.on("new-connection", function (user)
+  { socket.broadcast.emit('new-connection', user );  }); 
+                
+                
+      socket.on('new-msg', data => { 
+      io.emit('new-msgg', 
+       {user: socket.id, msg:data.msg}); 
+      
+      });
+      
+      }); 
+      http.listen(port, () => {
+           console.log(`Socket.IO server running at http://localhost:${port}/`);
+           
+           });

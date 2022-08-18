f3e960cf790c4a3e363fed87ee37dd1a3542233d
sumitrdic <33592600+sumitrdic@users.noreply.github.com>
2022-08-18 14:28:06 +0530

    Updete index.js
    Dbhs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000; 
-app.use(express.static(__dirname + '/Myweb/'));
+app.use(express.static(__dirname + '/'));
     
  io.on('connection', (socket) => { 
  
